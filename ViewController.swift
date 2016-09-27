dd0fa73d06a01d5cb0a96c9c8f1bc9b73ed63a87
jajuki13 <khkim1776@gmail.com>
2016-09-26 21:52:16 -0400

    iboutlets and actions learned

--- ViewController.swift
+++ ViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    All IBOutlets are weak var's. ARC. Instances to be cleaned.
+//    IBOutlets' types always ends in exclamation marks, optionals. ex: UILabel!
+    @IBOutlet weak var textDisplay: UILabel!
+
+    @IBAction func helloWorld(_ sender: AnyObject) {
+        print("Pressed the \"Hello World\" button and it changed \"Zzz...\" to \"Hello World!\"")
+        textDisplay.text = "Hello World!"
+    }
+    
+    @IBAction func goodbyeWorld(_ sender: AnyObject) {
+        print("Pressed the \"Good Bye World\" button and it changed \"Zzz...\" to \"Good Bye World!\"")
+        textDisplay.text = "Good Bye World!"
+    }
+    
+    @IBAction func unicorn(_ sender: AnyObject) {
+        print("Pressed the \"Unicorn\" button and it changed \"Zzz...\" to \"Unicorn!\"")
+        textDisplay.text = "🦄"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
     }
 
     override func didReceiveMemoryWarning() {
